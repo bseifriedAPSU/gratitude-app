7a414affa0a8db101421ab5071f6f2e79d772c38
bseifriedAPSU <125502167+bseifriedAPSU@users.noreply.github.com>
2023-10-15 21:41:33 -0500

    added useNavigate hook to journalEntryCreation Button

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,19 @@
 import Header from "../../components/Header"
-import "./home.css"
 import Wordcloud from "../../components/Wordcloud"
-
+import { useNavigate } from 'react-router-dom'
+import "./home.css"
 
 
 export default function Home() {
- 
+ const navigate = useNavigate()
 
     return (
         <div className="home">
             <Header />
             <Wordcloud />
 
-            <button /* On CLick HERE*/>
-                Create New Journal Entry Button 
-            </button>
+            <button onClick={() => navigate('/journalEntryCreation')}>Create New Journal Entry Button</button>
+
             <input className="homeSearchBar"
                 placeholder="Home Page Search Bar..."
                     /*on change HERE*/ />
