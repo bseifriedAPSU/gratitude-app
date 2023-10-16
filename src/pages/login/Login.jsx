93e750f5ea161b70aec0635a6f2f40a930139bb3
bseifriedAPSU <125502167+bseifriedAPSU@users.noreply.github.com>
2023-10-16 11:25:56 -0500

    nav and hamburger files added

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,12 +1,12 @@
 import "./login.css"
-
+import Header from "../../components/Header"
 export default function Login() {
 
     return (
         <div className="login">
-        <h1>Gratitude++ Journal</h1>
+        <Header />
         
-            <button className="login-with-google-btn" /* on Click Here*/>
+            <button className="button" /* on Click Here*/>
                 Sign in with Google
             </button>
         </div>
