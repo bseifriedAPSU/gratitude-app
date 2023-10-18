9691ca0338cf7de377303e9d4f1c784b6497e7a6
bseifriedAPSU <125502167+bseifriedAPSU@users.noreply.github.com>
2023-10-18 15:14:53 -0500

    Adding to NewUser Page

---
+++ src/components/UserSettings.jsx
@@ -0,0 +1,15 @@
+
+
+export default function UserSettings() {
+
+    return (
+        <div className="UserImage">
+            <img src="images/manAvatar1.png" alt="Dude" />
+            <input></input>
+            <button></button>
+            <line></line>
+        </div>
+
+    )
+}
+
--- src/pages/newUser/NewUser.jsx
+++ src/pages/newUser/NewUser.jsx
@@ -1,10 +1,13 @@
 import "./newUser.css"
 
+import UserSettings from "../../components/UserSettings"
+
 export default function NewUser() {
 
     return (
         <div className="newUser">
             New User Page
+            <UserSettings />
         </div>
 
     )
