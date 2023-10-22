6c4297b8b5f6616258f61f2250b920f5d665ce19
bseifriedAPSU <125502167+bseifriedAPSU@users.noreply.github.com>
2023-10-22 18:43:33 -0500

    Added Avatar Images to Public folder for reference and future use

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -8,15 +8,12 @@ export default function TopBar() {
             <div className="topLeft">
               
                 <div className="userInfo">
-                   <img className="userBannerImage" src="images/manAvatar1.png" alt="Dude" />
+                   <img className="userBannerImage" src="images/avatarImage1.png" alt="UserBannerImage" />
                     <div className="userInfoUsername">Username</div>
                 </div>
             </div>
             
             <div className="topCenter">  
-            
-           
-
             </div>
             <div className="topRight"><Nav /></div>
           
