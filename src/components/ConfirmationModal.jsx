e1d67a1d4f4ccb2b96f81ab8a7aa499481d2ac11
bseifriedAPSU <125502167+bseifriedAPSU@users.noreply.github.com>
2023-10-29 13:00:26 -0500

    Created Confirmation Modal (not Implemented)

---
+++ src/components/ConfirmationModal.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './confirmationModal.css';
+
+const ConfirmationModal = ({ isOpen, message, onConfirm, onCancel }) => {
+    if (!isOpen) return null;
+
+    return (
+        <div className="modal-container">
+            <div className="modal-background"></div>
+            <div className="modal-content">
+                <p className="modal-message">{message}</p>
+                <div className="modal-buttons">
+                    <button className="confirm-button" onClick={onConfirm}>
+                        Confirm
+                    </button>
+                    <button className="cancel-button" onClick={onCancel}>
+                        Cancel
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ConfirmationModal;
