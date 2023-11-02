477ae12bb6f3499677f7c57d9193369899965b3b
bseifriedAPSU <125502167+bseifriedAPSU@users.noreply.github.com>
2023-11-02 17:53:42 -0500

    Added base wordcloud from reac-wordcloud library resolved peer dependencies

--- src/components/Wordcloud.jsx
+++ src/components/Wordcloud.jsx
@@ -1,16 +1,36 @@
 import "./wordcloud.css"
+import React from 'react';
+import ReactWordcloud from 'react-wordcloud';
+
 
 export default function Wordcloud() {
 
+    const words = [
+        //TESTING VALUES
+        { text: 'puppies', value: 100 },
+        { text: 'tacos', value: 95 },
+        { text: 'trees', value: 90 },
+        { text: 'sunset', value: 90 },
+        { text: 'family', value: 85 },
+        { text: 'coffee', value: 70 },
+        { text: 'health', value: 40 },
+        { text: 'socks', value: 35 },
+        { text: 'home', value: 85 },
+        { text: 'kittens', value: 25 },
+        
+    ];
+
+    const options = {
+        rotations: 2,
+        rotationAngles: [0, 90],
+        fontSizes: [20, 60],
+      
+    };
     return (
-        <div className="wordcloud">
-            <h2>WORLDCLOUD COMPONENT</h2>
-            <h2>WORLDCLOUD COMPONENT</h2>
-            <h2>WORLDCLOUD COMPONENT</h2>
-            <h2>WORLDCLOUD COMPONENT</h2>
-            <h2>WORLDCLOUD COMPONENT</h2>
-
+        <div className="wordcloud-container">
+            <ReactWordcloud words={words} options={options} />
         </div>
+    );
 
-    )
-}
+};
+
