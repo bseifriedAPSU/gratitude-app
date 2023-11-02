bd8982f36ac5cb8c58448429c4dfb00173f3ecec
bseifriedAPSU <125502167+bseifriedAPSU@users.noreply.github.com>
2023-11-02 12:09:47 -0500

    Commiting to pull

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,21 +3,24 @@ import React, { useState, useEffect } from 'react'
 import { signIn, userAccountCheck } from "./../../firebase/database.js"
 import { auth } from './../../firebase/firebaseConfig.js'
 import Header from "../../components/Header"
+
 export default function Login() {
 
     const [user, setUser] = useState(null);
+
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged((user) => {
             if (user) {
                 setUser(user);
                 localStorage.setItem('uid', auth.currentUser.uid);
-                console.log(userAccountCheck(auth.currentUser.uid));
                 if (userAccountCheck(auth.currentUser.uid) === true) {
                     window.location.href = "/newUser";
+
                 }
                 else {
                     window.location.href = "/home";
+              
                 }
             }
             else {
@@ -31,8 +34,9 @@ export default function Login() {
 return (
     <div className="login">
 
+    <div className="logoImage">
         <Header />
-
+        </div>
         <button className="login-with-google-btn" onClick={signIn}>
             Sign in with Google
         </button>
