2839a840aa607b18720fb838204e90a95cd86f36
bseifriedAPSU <125502167+bseifriedAPSU@users.noreply.github.com>
2023-11-07 12:36:59 -0600

    added search bar but not pulling from database correctly (not fully styled either)

--- src/components/HomeSearchBar.jsx
+++ src/components/HomeSearchBar.jsx
@@ -1,27 +1,62 @@
 import "./homeSearchBar.css";
+import { db } from '../firebase/firebaseConfig';
 import React, { useState } from 'react';
 
-
 export default function HomeSearchBar() {
-    const [query, setQuery] = useState("")
 
-    function search(e) {
-        e.preventDefault()
-        setQuery(e.target.value)
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
 
-     //...
-    }
 
+
+    //MY HANDLE IS NOT WORKING CORRECTLY
+    
+    const handleSearch = async () => {
+        if (searchTerm.trim() !== '') {
+            const postRef = db.collection('posts');
+            const snapshot = await postRef.where('headline', '>=', searchTerm).get();
+
+            if (snapshot.empty) {
+                console.log("No Match");
+                setSearchResults([]);
+                return;
+            }
+
+            const results = [];
+            snapshot.forEach((doc) => {
+                results.push({ id: doc.id, ...doc.data() });
+            });
+            setSearchResults(results);
+        }
+    };
+
+
+
+    //THIS IS MY SEARCH BAR
     return (
-        <div className="w-full max-w-xl flex mx-auto p-20 text-xl">
+        <div className="homeSearchBarContainer">
+        <form>
             <input
                 type="text"
-                className="homeSearchInput"
-                placeholder="Search"
-                onChange={search}
-                value={query}   
-            />   
-           
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                placeholder="Search Your Entries"
+                />  
+                <button type="submit" onClick={handleSearch} >Search</button >
+            </form>
+
+
+            { /* THIS IS THE PROTOTYPE FOR DISPLAYING THAT WILL NEED TO RENDER ON FilteredArticleView.jsx Page*/ }
+            <div>
+                {searchResults.map((post) => (
+                <div key={post.id}>
+                    <h3>{post.headline}{post.date}</h3>
+                </div>
+            ))}
+            </div> 
         </div>
+       
     );
 };
+
+
